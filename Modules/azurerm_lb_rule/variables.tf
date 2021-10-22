a7870da8b0257a2bf313f17c50a3b300ff838ba3
harbhask <42581737+harbhask@users.noreply.github.com>
2021-10-22 12:16:39 +0530

    Create variables.tf

---
+++ Modules/azurerm_lb_rule/variables.tf
@@ -0,0 +1,27 @@
+variable "name" {
+    type = string
+}
+
+variable "lb_name" {
+    type = string
+}
+
+variable "lb_resource_group_name" {
+    type = string
+}
+
+variable "protocol" {
+    type = string
+}
+
+variable "frontend_name" {
+    type = string
+}
+
+variable "frontend_port" {
+    type = string
+
+}
+variable "backend_port" {
+    type = string
+}
