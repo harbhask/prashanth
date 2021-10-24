bb3ceb93fc7f6d5a97c021b16cb09869fed5b859
harbhask <42581737+harbhask@users.noreply.github.com>
2021-10-25 00:58:13 +0530

    Create variables.tf

---
+++ Modules/azurerm_virtual_machine_extension/Microsoft_EnterpriseCloud_Monitoring/variables.tf
@@ -0,0 +1,26 @@
+variable "vm_id" {
+    type    = string
+    default = ""
+}
+
+variable "vm_hostname" {
+    type    = string
+    default = ""
+}
+
+variable "vm_resource_group_name" {
+    type    = string
+    default = ""
+}
+
+variable "isLinuxVM" {
+    type        = bool
+}
+
+variable "law_name" {
+    type        = string
+}
+
+variable "law_resource_group_name" {
+    type        = string
+}
