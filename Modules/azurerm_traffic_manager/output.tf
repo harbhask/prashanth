7be5796d3edad66c67b4600dc6e0fae75dbbd0cc
harbhask <42581737+harbhask@users.noreply.github.com>
2021-10-28 13:17:11 +0530

    Create output.tf

---
+++ Modules/azurerm_traffic_manager/output.tf
@@ -0,0 +1,15 @@
+output "traffic_manager_profile_name" {
+    value = azurerm_traffic_manager_profile.traffic_manager_profile.name
+}
+
+output "traffic_manager_profile_id" {
+    value = azurerm_traffic_manager_profile.traffic_manager_profile.name
+}
+
+# output "traffic_manager_endpoint_name" {
+#     value = azurerm_traffic_manager_endpoint.traffic_manager_endpoint.id
+# }
+
+# output "traffic_manager_endpoint_name" {
+#     value = azurerm_traffic_manager_endpoint.traffic_manager_endpoint.name
+# }
